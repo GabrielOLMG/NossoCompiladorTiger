5058b43a004556861ce0f428addcee485f4cb1ac
GabrielOLMG <35979081+GabrielOLMG@users.noreply.github.com>
2022-01-10 16:06:28 +0000

    Add files via upload

--- new/codegenerator.c
+++ new/codegenerator.c
@@ -1,15 +1,19 @@
 #include <stdint.h>
 #include "estruturas.h" 
 #include "funcoes.h"
+void transExp(Exp exp,int dest);
 
 typedef intptr_t Addr; // endereço (inteiro ou apontador)
 typedef enum { MOVE, MOVEI, OPI, OP_, LABEL, JUMP, COND } Opcode; // código da instrução
 typedef struct {
-        Opcode opcode;
-        operacoes op_;
+    Opcode opcode;
+    Opcode opcode2;
+    Opcode opcode3;
+    operacoes op_;
+    ties_cond tc_;
 } Codigo;
 
-typedef struct { 
+typedef struct {
     Codigo codigo;
     Addr arg1, arg2, arg3;
 } Instr; //opcode = MOVEI arg1 = dest arg2 = exp->val arg3 = NULL;
@@ -23,7 +27,7 @@ int newLabel () {
     return label_count ++;
 }
 
-
+/*CADA EMIT PRECISA RETORNAR A STRUCT INSTR, que sera completada se necessaria depois*/
 
 void emit1(Codigo opcode_,int t1){
     switch(opcode_.opcode){
@@ -62,12 +66,12 @@ void emit3(Codigo opcode_,int dest,int t1, int t2){
             break;
         case OPI:
             /*criar struct Instr*/
-            printf("OPI T%d T%d\n",dest,t1); 
+            printf("OPI T%d T%d\n",dest,t1);
             break;
         case COND:
             printf("COND ");
             printaEnumOp(opcode_.op_);
-            printf("T%d T%d \n", t2,t1);
+            printf("T%d T%d ", t2,t1);
 
         default:
             break;
@@ -75,40 +79,56 @@ void emit3(Codigo opcode_,int dest,int t1, int t2){
 }
 
 
-//Opcode opcode_ties_cond;
 //Opcode opcode_io;
 
 Codigo opcode_operacoes(operacoes op){
-    Codigo c;
-    c.op_=op;
-	switch(op){
-		case ASSERT:
-            c.opcode = MOVE;
-			return c;
-        case EXP_SEQ:
-            printf("FAZER");
-            c.opcode = MOVE;
-			return c;
-        case VAR_:
-            printf("FAZER");
-            c.opcode = MOVE;
-			return c;
-        case MINUS:
-        case TIME:
-        case DIV:
-        case MOD:
-        case PLUS:
-            c.opcode = OP_;
-            return c;	
-        default:
-            c.opcode = COND;
-            return c;
-            break;
-	}
-    
+  Codigo c;
+  c.op_ = op;
+  switch(op){
+    case ASSERT:
+      c.opcode = MOVE;
+      return c;
+    case EXP_SEQ:
+      printf("FAZER");
+      c.opcode = MOVE;
+      return c;
+    case VAR_:
+      printf("FAZER");
+      c.opcode = MOVE;
+      return c;
+    case MINUS:
+    case TIME:
+    case DIV:
+    case MOD:
+    case PLUS:
+      c.opcode = OP_;
+      return c;
+    default:
+      c.opcode = COND;
+      c.opcode2 = LABEL;
+      return c;
+      break;
+  }
+}
+Opcode opcode_ties_cond(ties_cond tc){
+  Codigo c;
+  c.tc_ = tc;
+  switch(tc){
+  }
 }
 
-
+void transCond(Exp exp,int dest, int label1,int label2){
+  int t1 = newTemp();
+  int t2 = newTemp();
+  transExp(exp->operacoes.left, t1);
+  transExp(exp->operacoes.right, t2);
+  Codigo c;
+  c.op_ = exp->operacoes.op;
+  c.opcode = COND;
+  c.opcode2 = LABEL;
+  emit3(c,dest,t1,t2);//uma parte da isntrução sera feita aqui
+  printf("label%d label%d \n",label1,label2); // subsitutir essa parte por colocar a label na instrução
+}
 
 void transExp(Exp exp,int dest) {
     Codigo c;
@@ -122,7 +142,6 @@ void transExp(Exp exp,int dest) {
             transExp(exp->operacoes.left, t1); // lado esquerdo
             transExp(exp->operacoes.right, t2); // lado direito
             emit3(opcode_operacoes(exp->operacoes.op), dest, t1, t2); // instrução final
-            
             break;
         case NUM:
             c.opcode = MOVEI;
@@ -130,18 +149,33 @@ void transExp(Exp exp,int dest) {
             return;
             break;
         case TC:
+            /*
+              if LEFT then MIDDLE else RIGHT
+              if LEFT then MIDDLE
+              while LEFT do MIDDLE
+            */
             t1 = newTemp();
             t2 = newTemp();
-            transExp(exp->operacoes.left, t1); // lado esquerdo da cond
-            transExp(exp->operacoes.right, t2); // lado direito da cond
-            emit3(opcode_operacoes(exp->operacoes.op), 1, t1, t2);
-            printf("L%d L%d\n",0,1);
-        /*
-        case ID:
-            //temp = lookup(id,table);
-            int temp = 3;
-                break;
-        */
+            t3 = newTemp();
+        
+            l1 = newLabel();
+            l2 = newLabel();
+            
+            transCond(exp->ties_cond.left, t1,l1,l2);
+            printf("LABEL label%d\n",l1);
+            transExp(exp->ties_cond.middle, t2);
+            printf("LABEL label%d\n",l2);
+            if(exp->ties_cond.right != NULL){
+              l3 = newLabel();
+              transExp(exp->ties_cond.right, t2);
+              printf("LABEL label%d\n",l3);
+              
+            }
+            //transExp(exp->operacoes.right, t3);
+            //emit3(c, dest, t1, t2);
+            //printf("LABEL label%d\n", l2);
+        
+            //printf("L%d L%d\n",0,1);
         default:
             break;
     }
@@ -156,8 +190,9 @@ int main(){
     // Exp multiplicar = mk_op(TIME,numero3,soma);
     Exp less_than = mk_op(LT,numero1,numero2);
     Exp if_else = mk_tc(IF_,less_than,numero1,NULL);
+    Exp while_ = mk_tc(WHILE_DO,less_than,numero1,NULL);
     int dest = newTemp();
-    transExp(if_else,dest);
+    transExp(while_,dest);
 
 
     return 0;
@@ -182,36 +217,4 @@ OP Plus "t1" "t2" "t3", OP Mult "x" "t0" "t1"], (4,0))
 */
 
 
-/*
-5 * (4+3)
-
-(1-) OP
-dest = 0
-esq (5)
-dir (4+3)
-emit3.1
-
-(2-) NUM 5
-dest = 1
-t1=5
-
-(3-) OP 4+3
-dest = 2
-esq 4
-dir 3
-emit3.2
-
-(4-) NUM 4
-T3 = 4
-
-(5-) NUM 3
-T4 = 3
-
-(6-) emit3.2 (4+3) (T3 + T4)
-T2 = T3 + T4
-
-(7-) emit3.1 t1*(T2)
-
-
-*/
 
